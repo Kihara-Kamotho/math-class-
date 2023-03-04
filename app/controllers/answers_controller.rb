d477f365ac62044f28d4372de486e98ce7e4ad32
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-04 07:45:55 +0300

    added question and answer controllers

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,49 @@
+class AnswersController < ApplicationController 
+
+  before_action :set_question, only: [:index, :new, :create] 
+
+  before_action :set_answer, except: [:index, :new, :create]
+
+  def index
+    @answers = @question.answers
+  end 
+
+  def new 
+    @answer = @question.answers.new
+  end 
+
+  def create 
+    @answer = @question.answers.build(answer_params)
+
+    if @answer.save 
+      # turbo_stream 
+    else 
+      render :new 
+    end 
+  end 
+
+  def show
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+  def destroy  
+  end 
+  
+  private 
+  def set_question
+    @question = Question.find_by(params[:question_id])
+  end 
+
+  def answer_params
+    params.require(:answer).permit(:body) 
+  end 
+
+  def set_answer 
+    @answer = Answer.find(params[:id])
+  end 
+end 
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,52 @@
+class QuestionsController < ApplicationController 
+
+  before_action :set_lesson, only: [:index, :new, :create] 
+
+  before_action :set_question, except: [:index, :new, :create]
+
+  def index 
+    @questions = @lesson.questions
+  end
+
+  def new 
+    @question = @lesson.questions.new
+  end
+
+  def create 
+    @question = @lesson.questions.build(question_params)
+
+    if @question.save 
+      # turbo_stream 
+    else 
+      render :new 
+    end 
+  end
+
+  def show
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+  def destroy 
+    if @question.destroy 
+      # turbo_stream 
+    end 
+  end 
+
+  private 
+  def set_lesson 
+    @lesson = Lesson.find_by(params[:lesson_id])
+  end
+
+  def question_params 
+    params.require(:question).permit(:title, :body) 
+  end 
+
+  def set_question 
+    @question = Question.find(params[:id])
+  end
+end
