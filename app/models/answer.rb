d0cd86e27b6265facb9e85700fa5c7f077bbe1f7
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-04 13:09:44 +0300

    added turbo stream for action cable broadcasts

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  after_create_commit -> {broadcast_append_to 'answers'}
+  after_update_commit -> {broadcast_replace_to 'answers'}
+  after_destroy_commit -> {broadcast_remove_to 'answers'}
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers
+
+  after_create_commit -> {broadcast_append_to 'questions'}
+  after_update_commit -> {broadcast_replace_to 'questions'}
+  after_destroy_commit -> {broadcast_remove_to 'questions'}
 end
