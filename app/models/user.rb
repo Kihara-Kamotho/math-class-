ffc4d2d5c13ab887465967847efd5d65b5ac5554
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 10:43:17 +0300

    formatting user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,18 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
+
+class User < ApplicationRecord # rubocop:disable Style/Documentation
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :subscriptions
   has_many :courses, through: :subscriptions
-  has_many :payments, through: :subscriptions 
-  has_many :notifications, as: :recepient 
-  
+  has_many :payments, through: :subscriptions
+  has_many :notifications, as: :recepient
+
   # check if a user is subscribed to a specified course
   def subscribed_to?(course)
-    courses.exists?(course.id) 
-  end 
+    courses.exists?(course.id)
+  end
 end
