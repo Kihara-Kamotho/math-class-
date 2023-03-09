db9ccc899151efcf65956ec7f3cf5cef1c25c24b
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 11:16:19 +0300

    added status col to subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_042153) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_081357) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_042153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "expires_at"
+    t.integer "status", default: 0, null: false
     t.index ["course_id"], name: "index_subscriptions_on_course_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
