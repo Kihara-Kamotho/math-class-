c7631282cb960bd5828a3eaf1c816032103d14e4
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 09:05:35 +0300

    formatted registrations ctrl

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
-class RegistrationsController < Devise::RegistrationsController 
+# frozen_string_literal: true
 
-  private 
-  def sign_up_params 
+class RegistrationsController < Devise::RegistrationsController  # rubocop:disable Style/Documentation
+  private
+
+  def sign_up_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :phone)
   end
 
-  def account_update_params 
+  def account_update_params
     params.require(:user).permit(:email, :password, :password_confirmation, :name, :phone)
   end
 end
