1352f6551dddfbce433385cc24a3a16048328c27
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 12:07:41 +0300

    added mpesa payment that tracks payment response from the api

--- app/jobs/mpesa_payment_job.rb
+++ app/jobs/mpesa_payment_job.rb
@@ -1,7 +1,17 @@
-class MpesaPaymentJob < ApplicationJob
+class MpesaPaymentJob < ApplicationJob # rubocop:disable Style/Documentation
   queue_as :default
 
-  def perform(*args)
+  def perform(subscription_id)
     # After payment has been successfully made, change state to active
+    subscription = Subscription.where(subscription_id:)
+    # payment processing
+    # if subscription payment has state true & code is not nil, then payment processing has been successful
+    if subscription.payment.state? && !subscription.payment.code.nil?
+      subscription.update(status: 'active')
+      # send notification
+    else
+      # Retry the job in case of any errors or failures
+      retry_job wait: 5.minutes, queue: :low_priority
+    end
   end
 end
