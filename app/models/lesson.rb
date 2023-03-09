9a10803d186e5073cbe2d32168b079b37328e3af
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 08:42:02 +0300

    formatted lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,11 +1,13 @@
-class Lesson < ApplicationRecord
+# frozen_string_literal: true
+
+class Lesson < ApplicationRecord # rubocop:disable Style/Documentation
   belongs_to :section
-  
+
   has_many :comments, dependent: :destroy
 
   has_many :questions
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 
-  has_one_attached :video 
+  has_one_attached :video
 end
