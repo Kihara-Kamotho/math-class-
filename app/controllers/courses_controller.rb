1e467638a7d3547c2d5b8052b511f6ce45cdf0c1
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-09 08:30:32 +0300

    formatted courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,16 @@
-class CoursesController < ApplicationController 
-  before_action :set_course, only: [:show, :edit, :update, :destroy] 
+# frozen_string_literal: true
 
-  def index 
-    # all courses 
+class CoursesController < ApplicationController  # rubocop:disable Style/Documentation
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # all courses
     @courses = Course.all.includes(:sections)
     @q = Course.ransack(params[:q])
     @pagy, @courses = pagy(@q.result(distinct: true), items: 7)
-  
-  end 
+  end
 
-  def new 
+  def new
     @course = Course.new
   end
 
@@ -17,50 +18,46 @@ class CoursesController < ApplicationController
     @course = Course.new(course_params)
 
     respond_to do |format|
-
-      if @course.save 
+      if @course.save
         format.turbo_stream
-        format.html { redirect_to course_path @course, flash[:notice] = 'Course created successfully.'} 
-      else 
-        render :new 
+        format.html { redirect_to course_path @course, flash[:notice] = 'Course created successfully.'}
+      else
+        render :new
       end
     end
   end
 
-  def show 
-  end
+  def show; end
 
-  def edit 
-  end 
+  def edit; end
 
-  def update 
-    respond_to do |format| 
-
-      if @course.update(course_params) 
-        format.turbo_stream 
-        format.html { redirect_to course_path @course, flash[:notice] = 'Course updated.' } 
-      else 
-        render :edit 
-      end 
-    end 
-  end 
-
-  def destroy 
-    respond_to do |format|  
-
-      if @course.delete 
-        format.html { redirect_to courses_path, flash[:notice] = 'Course deleted.' }  
+  def update
+    respond_to do |format|
+      if @course.update(course_params)
         format.turbo_stream
-      end 
+        format.html { redirect_to course_path @course, flash[:notice] = 'Course updated.' }
+      else
+        render :edit
+      end
     end
   end
 
-  private 
-  def course_params 
-    params.require(:course).permit(:title, :description, :amount) 
+  def destroy
+    respond_to do |format|
+      if @course.delete
+        format.html { redirect_to courses_path, flash[:notice] = 'Course deleted.' }
+        format.turbo_stream
+      end
+    end
   end
 
-  def set_course 
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :amount)
+  end
+
+  def set_course
     @course = Course.find(params[:id])
-  end 
+  end
 end
