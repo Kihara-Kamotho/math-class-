5d34ba3f0c7ef42020888335abac3d2ef56593a2
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-03-16 16:56:15 +0300

    user cannot view unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
   include Pagy::Backend
   include SubscriptionHelper
+
+  protected
+  
+  def authorize
+    authenticate_user!
+  end
 end
