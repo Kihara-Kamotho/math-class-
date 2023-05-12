e3f53ea14659142bf1a8cc1d8a52284a1a3648a7
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-05-12 16:15:03 +0300

    Add callback action
    Ensures that the user doesnt see the home page when they log out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController # rubocop:disable Style/Documentation
+  before_action :authenticate_user!
   def index
     @courses = Course.all
   end
